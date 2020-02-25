f328c7d877ce98809a94e2531e7667f73bf74983
zacharyedwardsdowns <zacharyedwardsdowns@gmail.com>
2020-02-25 12:42:15 -0600

    Registration Service

---
+++ registration/src/test/java/com/microbank/service/registration/RegistrationServiceTest.java
@@ -0,0 +1,11 @@
+package com.microbank.service.registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationServiceTest {
+
+  @Test
+  void contextLoads() {}
+}
