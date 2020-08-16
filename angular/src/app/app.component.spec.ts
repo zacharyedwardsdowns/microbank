fb02b8c6f0616e83e18fc488094da36fe7cb9d9f
Zachary Edwards Downs <zacharyedwardsdowns@gmail.com>
2020-08-16 12:09:36 -0500

    Angular Material + format.sh

--- angular/src/app/app.component.spec.ts
+++ angular/src/app/app.component.spec.ts
@@ -1,9 +1,9 @@
-import { TestBed, async } from "@angular/core/testing";
-import { RouterTestingModule } from "@angular/router/testing";
-import { AppComponent } from "./app.component";
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
 
-describe("AppComponent", () => {
-  let title = "microbank-ui";
+describe('AppComponent', () => {
+  let title = 'microbank-ui';
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -12,7 +12,7 @@ describe("AppComponent", () => {
     }).compileComponents();
   }));
 
-  it("should create the app", () => {
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
@@ -24,11 +24,11 @@ describe("AppComponent", () => {
     expect(app.title).toEqual(title);
   });
 
-  it("should render title", () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector(".content span").textContent).toContain(
+    expect(compiled.querySelector('.content span').textContent).toContain(
       `${title} app is running!`
     );
   });
