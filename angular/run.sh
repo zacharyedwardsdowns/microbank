23c76adf6b117648933f8048a85f8e268e8d9af1
Zachary Edwards Downs <zacharyedwardsdowns@gmail.com>
2021-05-30 16:58:09 -0400

    LetsEncrypt Https

--- angular/run.sh
+++ angular/run.sh
@@ -1,1 +1,1 @@
-docker run -d -p "4200:80" --name "microbank-angular" registry.gitlab.com/zacharyedwardsdowns/micro-bank/angular
+docker run -d -p "80:80" -p "443:443" --name "microbank-angular" registry.gitlab.com/zacharyedwardsdowns/micro-bank/angular
